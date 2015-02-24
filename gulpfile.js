045f6ff4fd218ec4218c54c5f860a47a72479d86
mrmrs <hi@mrmrs.cc>
2015-02-24 13:19:42 -0800

    Updating site with massive changes.
    - Converts to jekyll site
    - Moves to rework
    - Adds ability to post shows

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,61 @@
+// Gulp tasks for Tachyons
 
-var gulp = require('gulp');
+// Load plugins
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    basswork = require('gulp-basswork'),
+    watch = require('gulp-watch'),
+    prefix = require('gulp-autoprefixer'),
+    //uncss = require('gulp-uncss'),
+    minifyCSS = require('gulp-minify-css'),
+    //sass = require('gulp-sass'),
+    size = require('gulp-size'),
+    rename = require('gulp-rename'),
+    csslint = require('gulp-csslint'),
+    css = require('css'),
+    browserSync = require('browser-sync'),
+    browserReload = browserSync.reload;
 
-var browserify = require('gulp-browserify');
-var cheerio = require('gulp-cheerio');
-var connect = require('gulp-connect');
-var prefix = require('gulp-autoprefixer');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglifyjs');
-var watch = require('gulp-watch');
-
-var markdown = require('gulp-markdown');
-
-var pygmentize = require('./docs/gulp/pygments');
-
-gulp.task('compilejs', function() {
-  gulp.src('./src/v-plangular.js')
-    .pipe(browserify())
-    .pipe(gulp.dest('./'))
-    .pipe(uglify())
-    .pipe(rename('v-plangular.min.js'))
-    .pipe(gulp.dest('./'));
-  gulp.src('./src/ng-plangular.js')
-    .pipe(gulp.dest('./'))
-    .pipe(uglify())
-    .pipe(rename('ng-plangular.min.js'))
-    .pipe(gulp.dest('./'));
+gulp.task('css', function() {
+  gulp.src('./styles/tachyons.css')
+    .pipe(basswork())
+    .pipe(size({gzip: false, showFiles: true, title:'basswork css'}))
+    .pipe(size({gzip: true, showFiles: true, title:'basswork gzipped css'}))
+    .pipe(gulp.dest('./css'))
+    .pipe(minifyCSS())
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(size({gzip: false, showFiles: true, title:'basswork minified'}))
+    .pipe(size({gzip: true, showFiles: true, title:'basswork minified'}))
+    .pipe(gulp.dest('./css'));
 });
 
 
-gulp.task('docs-pygmentize', function() {
-  gulp.src('./docs/vuejs/src/*.html')
-    .pipe(gulp.dest('./docs/vuejs/partials'))
-    .pipe(pygmentize())
-    .pipe(rename(function(path) { path.basename += '-code', path.extname = '.html' }))
-    .pipe(gulp.dest('./docs/vuejs/partials'));
-  gulp.src('./docs/angular/src/*.html')
-    .pipe(gulp.dest('./docs/angular/partials'))
-    .pipe(pygmentize())
-    .pipe(cheerio(function($) {
-      $('.highlight').attr('ng-non-bindable', '');
-    }))
-    .pipe(rename(function(path) { path.basename += '-code', path.extname = '.html' }))
-    .pipe(gulp.dest('./docs/angular/partials'));
+// Initialize browser-sync which starts a static server also allows for
+// browsers to reload on filesave
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./_site"
+        }
+    });
 });
 
-gulp.task('docs-sass', function() {
-  gulp.src('./docs/src/autobass.scss')
-    .pipe(sass({ options: { outputStyle: 'compressed' } }))
-    .pipe(prefix())
-    .pipe(gulp.dest('./docs'));
+// Function to call for reloading browsers
+gulp.task('bs-reload', function () {
+    browserSync.reload();
 });
 
-// Currently unused
-//gulp.task('docs-md', function() {
-//  var options = require('./docs/gulp/md-options');
-//  gulp.src('./docs/examples/**/*.md')
-//    .pipe(markdown(options))
-//    .pipe(gulp.dest('./docs/examples'));
-//});
+/*
+   DEFAULT TASK
 
-gulp.task('docs-js', function() {
-  gulp.src('./docs/vuejs/src/*.js')
-    .pipe(browserify())
-    .pipe(uglify())
-    .pipe(gulp.dest('./docs/vuejs'));
-  gulp.src('./docs/angular/src/*.js')
-    .pipe(browserify())
-    .pipe(uglify())
-    .pipe(gulp.dest('./docs/angular'));
+ • Process sass then auto-prefixes and lints outputted css
+ • Starts a server on port 3000
+ • Reloads browsers when you change html or sass files
+
+*/
+gulp.task('default', ['css', 'bs-reload', 'browser-sync'], function(){
+  gulp.start(['css', 'bs-reload']);
+  gulp.watch('styles/*', ['css']);
+  gulp.watch(['_site/*',], ['bs-reload']);
 });
-
-gulp.task('server', function() {
-  connect.server();
-});
-
-gulp.task('default', ['compilejs', 'docs-sass', 'docs-js', 'docs-pygmentize', 'server'], function() {
-  gulp.watch(
-    ['./src/**/*', './docs/src/**/*', './docs/vuejs/src/**/*', './docs/angular/src/**/*'],
-    ['compilejs', 'docs-sass', 'docs-js', 'docs-pygmentize']
-  );
-});
-
 
